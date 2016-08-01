5ca6e67ad3725c7f05b60349dfe61064101dd29b
luisalmeida <luis.almeida@realbase.pt>
2016-08-01 17:23:10 +0100

    everything

---
+++ connection/conn.php
@@ -0,0 +1,16 @@
+<?php
+$link = mysqli_connect("localhost", "root", "root", "prbdb");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+ //mysqli_set_charset($link,"utf8");
+// echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+// echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+
+?>
