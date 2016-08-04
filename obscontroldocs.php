a477b1de4b68c2741e6f4a277986c6161ce7ea53
luisalmeida <luis.almeida@realbase.pt>
2016-08-04 15:28:14 +0100

    obscontrols docs e deleterosto

--- obscontroldocs.php
+++ obscontroldocs.php
@@ -307,7 +307,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               											closedir($dh);
               										}
               									}
-              									
+
               									?>
               								</tbody>
               							</table>
@@ -342,51 +342,65 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <h3 class="box-title">Obsoletos</h3>
               </div><!-- /.box-header -->
               <div class="box-body">
-              	<div class="box-group" id="accordion">
-              		<!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
-              		<div class="panel box box-primary">
-              			<div class="box-header with-border">
-              				<h4 class="box-title">
-              					<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" class="">
-              						Instruções de trabalho
-              					</a>
-              				</h4>
-              			</div>
-              			<div id="collapseOne" class="panel-collapse collapse in" aria-expanded="true">
-              				<div class="box-body">
-              					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              				</div>
-              			</div>
-              		</div>
-              		<div class="panel box box-primary">
-              			<div class="box-header with-border">
-              				<h4 class="box-title">
-              					<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed" aria-expanded="false">
-              						Modelos
-              					</a>
-              				</h4>
-              			</div>
-              			<div id="collapseTwo" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
-              				<div class="box-body">
-              					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              				</div>
-              			</div>
-              		</div>
-              		<div class="panel box box-primary">
-              			<div class="box-header with-border">
-              				<h4 class="box-title">
-              					<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="collapsed" aria-expanded="false">
-              						Procedimentos
-              					</a>
-              				</h4>
-              			</div>
-              			<div id="collapseThree" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
-              				<div class="box-body">
-              					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              				</div>
-              			</div>
-              		</div>
-              	</div>
+              	<?php 
+
+              	$dir = "files/obsoletos/controldocs/prs/";
+
+              	$filesQuantity = new FilesystemIterator($dir, FilesystemIterator::SKIP_DOTS);
+              						//printf("There were %d Files", iterator_count($fi));
+
+              	?>
+
+              	<h5>Nº de ficheiros - <?php echo iterator_count($filesQuantity)." ficheiro(s)";?></h5>
+
+              	<div class="table-responsive">
+              							<table class="table no-margin">
+              								<thead>
+              									<tr>
+              										<th>Nome documento</th>
+              										<th>Extensão</th>
+              										<th>Status</th>
+              										
+              									</tr>
+              								</thead>
+              								<tbody>
+              									<?php
+
+              									$dir = "files/obsoletos/controldocs/prs/";
+
+									// Open a directory, and read its contents
+              									
+              									if (is_dir($dir)){
+
+              										if ($dh = opendir($dir)){
+              											$count = 0;
+              											while (($file = readdir($dh)) !== false){
+
+              											        													
+	              												$ext = substr($file, strpos($file, ".") + 1);
+	              												$mark    = '.';
+	              												$fileName =  strstr($file, $mark, true);
+	              												
+	              					
+	              											
+
+              												?>
+              												<tr>
+              													<td><?php echo $fileName;?></td>
+              													<td><?php echo $ext;?></td>
+              													<td><a href="<?php echo $dir.$file; ?>"><span class="label label-info">Ver / Download</span></a></td>
+
+              												</tr>
+              												<?php	
+              											}
+              											closedir($dh);
+              										}
+              									}
+              									
+              									?>
+              								</tbody>
+              							</table>
+              						</div><!-- /.table-responsive -->
               </div><!-- /.box-body -->
           </div><!-- /.box -->
       </div>
