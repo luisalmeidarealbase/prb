04887c58ceb1a36c794af983f5f0e5e6a39afaf4
luisalmeida <luis.almeida@realbase.pt>
2016-10-11 14:37:56 +0100

    removed the e

--- edicao-controlodocumental.php
+++ edicao-controlodocumental.php
@@ -76,7 +76,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Logo -->
       <a href="starter.php" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>P</b>RBe</span>
+        <span class="logo-mini"><b>P</b>RB</span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><b>Portal</b>Realbase</span>
       </a>
