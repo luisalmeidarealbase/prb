eb439e0aaf2876908e8e8b0f792b8d484e858fa1
luisalmeida <luis.almeida@realbase.pt>
2016-11-11 12:34:10 +0000

    page table-control-docs created

--- connection/conn.php
+++ connection/conn.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("localhost", "root", "root", "prbgitdb");
+$link = mysqli_connect("localhost", "root", "root", "prbnov");
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
